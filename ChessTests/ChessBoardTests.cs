3184df16946a0e7074bafe45a782f82a8447c01c
Samuel 'Sam The Man' Fleming <rollondgerweb@gmail.com>
2015-09-03 17:15:16 -0600

    added in is legal board checking tests

--- ChessTests/ChessBoardTests.cs
+++ ChessTests/ChessBoardTests.cs
@@ -60,7 +60,67 @@ namespace ChessTests
 				}
 			}
 
-			
+			protected class Given_Empty_ChessBoard : ChessBoardSuccessContext
+			{	 
+				protected override void Given()
+				{  
+					base.Given();
+				}
+
+				protected override void When()
+				{
+					base.When();
+				}
+
+				[Test]
+				public void When_IsLegalBoardPosition_0_0_ShouldBe_Legal()
+				{
+					var isLegal = ChessBoard.IsLegalBoardPosition(0, 0);
+					Assert.That(isLegal, Is.True);
+				}
+
+				[Test]
+				public void When_IsLegalBoardPosition_5_5_ShouldBe_Legal()
+				{
+					var isLegal = ChessBoard.IsLegalBoardPosition(5, 5);
+					Assert.That(isLegal, Is.True);
+				}
+
+				[Test]
+				public void When_IsLegalBoardPosition_11_5_Should_Not_Be_Legal()
+				{
+					var isLegal = ChessBoard.IsLegalBoardPosition(11, 5);
+					Assert.That(isLegal, Is.False);
+				}
+
+				[Test]
+				public void When_IsLegalBoardPosition_0_9_Should_Not_Be_Legal()
+				{
+					var isLegal = ChessBoard.IsLegalBoardPosition(0, 9);
+					Assert.That(isLegal, Is.False);
+				}
+
+				[Test]
+				public void When_IsLegalBoardPosition_11_0_Should_Not_Be_Legal()
+				{
+					var isLegal = ChessBoard.IsLegalBoardPosition(11, 0);
+					Assert.That(isLegal, Is.False);
+				}
+
+				[Test]
+				public void When_IsLegalBoardPosition_negitive1_5_Should_Not_Be_Legal()
+				{
+					var isLegal = ChessBoard.IsLegalBoardPosition(-1, 5);
+					Assert.That(isLegal, Is.False);
+				}
+
+				[Test]
+				public void When_IsLegalBoardPosition_5_negitive1_Should_Not_Be_Legal()
+				{
+					var isLegal = ChessBoard.IsLegalBoardPosition(5, -1);
+					Assert.That(isLegal, Is.False);
+				}
+			}
 		}
 	}
 }
